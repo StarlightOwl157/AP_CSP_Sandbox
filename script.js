15edcbe651eacb9283118859f11c460035c79b03
StarlightOwl157 <s1016395@student.knoxschools.org>
2026-03-20 18:03:19 +0000

    imported dayjs properly

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-import * as dayjs from 'dayjs'
-console.log(dayjs())
-// var now = dayjs().format('MM-DD-YYYY dddd');
-// var sec = dayjs().second() // => new Date().getSeconds()
-// var min = dayjs().minute()
-// var hour = dayjs().hour()
-// var dayofweek = dayjs().day()
-// 
-// console.log(now)
-// console.log(hour + ":" + min + ":" + sec)
+// import * as dayjs from 'dayjs'
+const dayjs = require('dayjs')
+// console.log(dayjs())
+var now = dayjs().format('MM-DD-YYYY dddd');
+var sec = dayjs().second() // => new Date().getSeconds()
+var min = dayjs().minute()
+var hour = dayjs().hour()
+var dayofweek = dayjs().day()
+
+console.log(now)
+console.log(hour + ":" + min + ":" + sec)
