85597132da4a63c55df094c980fde2553167e1f3
StarlightOwl157 <s1016395@student.knoxschools.org>
2026-03-20 17:58:27 +0000

    installled dayjs Library

---
+++ script.js
@@ -0,0 +1,10 @@
+import * as dayjs from 'dayjs'
+console.log(dayjs())
+// var now = dayjs().format('MM-DD-YYYY dddd');
+// var sec = dayjs().second() // => new Date().getSeconds()
+// var min = dayjs().minute()
+// var hour = dayjs().hour()
+// var dayofweek = dayjs().day()
+// 
+// console.log(now)
+// console.log(hour + ":" + min + ":" + sec)
