5ac75c12920d109faeacf8ad22a561da83ec1ab0
StarlightOwl157 <s1016395@student.knoxschools.org>
2026-03-20 18:34:11 +0000

    added basic html elements

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-// import * as dayjs from 'dayjs'
-const dayjs = require('dayjs')
-// console.log(dayjs())
-var now = dayjs().format('MM-DD-YYYY dddd');
-var sec = dayjs().second() // => new Date().getSeconds()
-var min = dayjs().minute()
-var hour = dayjs().hour()
-var dayofweek = dayjs().day()
 
-console.log(now)
-console.log(hour + ":" + min + ":" + sec)
+function find_date() {
+    // import * as dayjs from 'dayjs'
+    const dayjs = require('dayjs')
+    // console.log(dayjs())
+    var now = dayjs().format('MM-DD-YYYY dddd');
+    var sec = dayjs().second() // => new Date().getSeconds()
+    var min = dayjs().minute()
+    var hour = dayjs().hour()
+    var dayofweek = dayjs().day()
+    document.getElementById(answer).innerText = `${dayofweek}`
+    console.log(now)
+    console.log(hour + ":" + min + ":" + sec)
+    console.log('hello')
+}
