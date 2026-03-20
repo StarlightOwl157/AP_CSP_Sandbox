d27c195ef180cfa3090d2793ff9f6066082214a8
StarlightOwl157 <s1016395@student.knoxschools.org>
2026-03-20 18:52:21 +0000

    working on creating answer in html

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ function find_date() {
     // import * as dayjs from 'dayjs'
     const dayjs = require('dayjs')
     // console.log(dayjs())
-    var now = dayjs().format('MM-DD-YYYY dddd');
-    var sec = dayjs().second() // => new Date().getSeconds()
-    var min = dayjs().minute()
-    var hour = dayjs().hour()
+    // var now = dayjs().format('MM-DD-YYYY dddd');
+    // var sec = dayjs().second() // => new Date().getSeconds()
+    // var min = dayjs().minute()
+    // var hour = dayjs().hour()
     var dayofweek = dayjs().day()
-    document.getElementById(answer).innerText = `${dayofweek}`
-    console.log(now)
-    console.log(hour + ":" + min + ":" + sec)
-    console.log('hello')
+    console.log(dayofweek)
+    document.getElementById('answer').innerText = "hello"
+    // console.log(now)
+    // console.log(hour + ":" + min + ":" + sec)
+    // console.log('hello')
 }
+
+ find_date()
